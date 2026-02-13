419ede54002b35f9390c12262818c505a8b5c9b6
Digvijay Dhadwal <149832475+Digvijay8580@users.noreply.github.com>
2026-02-13 13:11:03 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,21 +1,23 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 from setuptools import setup
-
 import pathlib
 
-# The directory containing this file
 HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "README.md").read_text()
+README = (HERE / "README.md").read_text() if (HERE / "README.md").exists() else "Web Vulnerability Scanner"
 
 setup(
-    name                =   "Onixscanner",
-    description         =   "Web Vulnerability Scanner.",
-    long_description    =   README,
-    long_description_content_type = "text/markdown",
-    url                 =   "https://github.com/Digvijay8580/OnixD",
-    py_modules          =   ['Onixscanner',],
-    install_requires    =   [],
+    name="OnixScanner",
+    version="1.0.0",
+    description="Simple Web Vulnerability Scanner for Students",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Digvijay8580/OnixD",
+    py_modules=['onixscanner'],
+    install_requires=[],
     python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            'onixscanner=onixscanner:main',
+        ],
+    },
 )
